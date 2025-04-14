587b728fd54c05c6678b1f45e37a7e8b15f404f1
Klim Korovkin <klimkorovkin@yandex.ru>
2025-04-14 13:05:06 +0300

    Development changes: UI updates and modifications

--- frontend/src/views/TodayView.jsx
+++ frontend/src/views/TodayView.jsx
@@ -2,9 +2,13 @@ import styles from './TodayView.module.css';
 
 const TodayView = () => {
   return (
-    <div className={styles.container}>
-      <span className={styles.placeholder}>Today</span>
-    </div>
+    <>
+      <div className={styles.gradient}></div>
+      <div className={styles.container}>
+        <div className={styles.resizeHandle}></div>
+        <h3 className={styles.title}>После этого</h3>
+      </div>
+    </>
   );
 };
 
